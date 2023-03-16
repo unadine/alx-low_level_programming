8c2321379704fa959d51e7eb678edbb2495c9d16
unadine <tesskay4@gmail.com>
2023-03-16 13:52:36 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /*a function to print a sentence */
-int main (void){
-	printf("with proper grammar, but the outcome is a piece of art,"\n");
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
