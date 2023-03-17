778f03e3f57fea43a435a4bed014c901f91c302a
unadine <tesskay4@gmail.com>
2023-03-17 11:25:39 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z' ; x++)
+	{
+		if (x == 'q' || x == 'e')
+		{
+			continue;
+		}
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}i
