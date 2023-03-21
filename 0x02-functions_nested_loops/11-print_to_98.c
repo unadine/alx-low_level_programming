bed02f881d78abe57794d8a466c9f02a5daef45d
unadine <tesskay4@gmail.com>
2023-03-21 12:29:21 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_to_98 - print n to 98 counts
+ *           separated by comma, followed
+ *           by space and number should be
+ *           printed in order
+ *
+ * @n: input
+*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d,", i);
+		}
+	} else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d,", i);
+		}
+	}
+	printf("\n");
+}
