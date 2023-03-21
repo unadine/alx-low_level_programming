114b1056e020e8939c0c4ea3e307a3c6ce4412ab
unadine <tesskay4@gmail.com>
2023-03-21 12:33:17 -0700

    debug task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,16 +14,16 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
 			printf("%d, ", i);
 		}
 	} else
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
 			printf("%d, ", i);
 		}
 	}
-	printf("\n");
+	printf("98\n");
 }
