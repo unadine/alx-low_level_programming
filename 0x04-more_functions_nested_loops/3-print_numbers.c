94c06042d6a87af211098b309ddb39390c9c687f
unadine <tesskay4@gmail.com>
2023-03-23 08:29:37 -0700

    fix task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,11 @@
  */
 void print_numbers(void)
 {
-	int num = 0;
+	int i;
 
-	do {
-		_putchar(num + 48);
-		num++;
-	} while (num >= 0 && num <= 9);
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
 	_putchar('\n');
 }
