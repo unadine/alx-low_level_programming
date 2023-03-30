b531eb7ef128f18a25f898a9f52b88eac4cafe97
unadine <tesskay4@gmail.com>
2023-03-30 14:00:42 -0700

    Print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_number - prints number
+ * @n:integer to convert to character
+ *
+ */
+void print_number(int n)
+{
+	unsigned int a;
+	int mult = 1;
+	unsigned int aCount;
+	int i;
+	int c = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n += 1;
+		n *= -1;
+		n++;
+	}
+	a = n;
+	aCount = n;
+
+	while (aCount > 0)
+	{
+		aCount /= 10;
+		c++;
+	}
+	for (i = 0; i < c - 1; i++)
+		mult *= 10;
+
+	for (i = 0; i < c; i++)
+	{
+		_putchar((abs / mult) + '0');
+		a = a % mult;
+		mult /= 10;
+	}
+}
