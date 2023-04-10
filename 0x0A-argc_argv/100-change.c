b83b405ac7e40e6de1f65d201cf9fa94df304971
unadine <tesskay4@gmail.com>
2023-04-10 14:14:45 -0700

    100-change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - coins to make change for an amount of money
+ * @argc: argument counter
+ * @argv: array of pointer to strings
+ *
+ * Return: 0 if no errors, else
+ */
+int main(int argc, char *argv[])
+{
+	int cents;
+	int coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	coins += cents / 25;
+	cents %= 25;
+	coins += cents / 10;
+	cents %= 10;
+	coins += cents / 5;
+	cents %= 5;
+	coins += cents / 2;
+	cents %= 2;
+	coins += cents;
+
+	printf("%d\n", coins);
+	return (0);
+}
