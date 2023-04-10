32d1a82563f33b598cd8b513ad25244215f1a162
unadine <tesskay4@gmail.com>
2023-04-10 12:35:23 -0700

    fixing bugs

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,11 +8,12 @@
  */
 int main(int argc, char *argv[])
 {
-	int i=0;
-   	while(i < argc)
-    	{
-        	printf("%s\n", argv[i]);
-        	i++;
-    	}
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
 	return (0);
 }
