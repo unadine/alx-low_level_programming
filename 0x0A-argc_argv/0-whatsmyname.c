36771ee71a53348075f53fe598fc01c6d448a2ea
unadine <tesskay4@gmail.com>
2023-04-10 12:19:17 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * @argc: argument counter
+ * @argv: array of pointer to strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i=0;
+   	while(i < argc)
+    	{
+        	printf("%s\n", argv[i]);
+        	i++;
+    	}
+	return (0);
+}
