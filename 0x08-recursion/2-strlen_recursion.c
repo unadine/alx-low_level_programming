cdc7d35cda66c0269d8fcab648a5d9d923d782d4
unadine <tesskay4@gmail.com>
2023-04-15 13:44:26 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * _strlen_recursion - returns stringlength
+ * @s: input string
+ * Return: void
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
+
