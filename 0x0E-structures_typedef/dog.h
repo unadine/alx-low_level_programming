25e90b3ca6acebd95ee69a28280b18ef262ab44a
unadine <tesskay4@gmail.com>
2023-04-17 13:19:34 -0700

    Task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - dog structure
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
